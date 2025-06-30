167a8cc2b17bdcdd395d3f00e99924510ce397df
Rohan Krishna R.S <160414868+RohankrishnaRS@users.noreply.github.com>
2025-06-30 11:11:34 +0530

    Add files via upload

---
+++ VirtualArtGallery-main project/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using VirtualArtGallery.Main;
+
+namespace VirtualArtGallery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainModule mainModule = new MainModule();
+            mainModule.Run();
+        }
+    }
+}
